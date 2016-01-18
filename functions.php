7c379a1454158b5a2afe12448303fffe913b76dc
Marcus Vinicius Gonçalves <marquinhusgoncalves@hotmail.com>
2016-01-18 18:52:02 -0200

    Atualizações

--- functions.php
+++ functions.php
@@ -85,33 +85,6 @@ set_post_thumbnail_size( 200, 200 );
 		}
 	}
 
-// Logo Admin
-  function my_login_logo() { ?>
-  <style type="text/css">
-    .login h1 a {
-        background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/logo.png);
-        background-size: 260px auto;
-        width: 320px;
-    }
-</style>
-<?php }
-add_action( 'login_enqueue_scripts', 'my_login_logo' );
+include_once('functions/personalizacao.php');
 
-// Link Logo
-function meu_wp_login_url() {
-    return get_bloginfo('url');
-}
-add_filter('login_headerurl', 'meu_wp_login_url');
-
-// Logo Title
-function meu_wp_login_title() {
-    return get_bloginfo('name');
-}
-add_filter('login_headertitle', 'meu_wp_login_title');
-
-// Texto admin rodape
-function remove_footer_admin () {
-    echo "Feito com o <3 por Mundo S.A";
-}
-add_filter('admin_footer_text', 'remove_footer_admin');
 ?>
--- page-a-mundo.php
+++ page-a-mundo.php
@@ -1,25 +1,24 @@
 <?php get_header(); ?>
-
-<div class = "equipe">
-	<div class = "container">
-		<div class = "col-xs-12 col-sm-3 equipe-effect">
-			<div class = "individuo">
-				<a href = "#" class = "equipe_mask equipe_mask_style" style = "display: table; width: 100%; height: 630px; position: absolute; z-index: 10">
-					<figcaption style = "display: table-cell; vertical-align: middle">
+<div class="equipe">
+	<div class="container">
+		<div class="col-xs-12 col-sm-3 equipe-effect">
+			<div class="individuo">
+				<a href="#" class="equipe_mask equipe_mask_style">
+					<figcaption style="display: table-cell; vertical-align: middle">
 						<img src="<?php bloginfo('stylesheet_directory');?>/img/equipe/icone_ju.png" style="margin-bottom: 90px">
 						<div class="wrap">
 							<p style="margin-bottom: 90px">Formada em administração de empresas pela universidade Uni A, com experiência em montadoras e outras agências. É sócia e responsável pelas áreas financeira e adminstrativa, e conselheira nos assuntos criativos de alguns trabalhos.</p>
-							<div class = "equipe-desc"><h2>Juliana Furini</h2><p>Sócia - Diretora Financeira</p></div>
+							<div class="equipe-desc"><h2>Juliana Furini</h2><p>Sócia - Diretora Financeira</p></div>
 						</div>
 					</figcaption>
 				</a>
-				<div class = "over-image ju" ></div>
+				<div class="over-image ju" ></div>
 			</div>
 		</div>
-		<div class = "col-xs-12 col-sm-3 equipe-effect">
-			<div class = "individuo">
-				<a href = "#" class = "equipe_mask" style = "display: table; width: 100%; height: 630px; position: absolute; z-index: 10">
-					<figcaption style = "display: table-cell; vertical-align: middle">
+		<div class="col-xs-12 col-sm-3 equipe-effect">
+			<div class="individuo">
+				<a href="#" class="equipe_mask">
+					<figcaption style="display: table-cell; vertical-align: middle">
 						<img src="<?php bloginfo('stylesheet_directory');?>/img/equipe/icone_ale.png" style="margin-bottom: 90px">
 						<div class="wrap">
 							<p style="margin-bottom: 90px">Trabalha com arte desde os 14 anos, passando por diversas áreas desde, silk screen, pré impressão, finalização, marketing até chegar em criação. É sócio e responsável pelos departamentos de atendimento e criação da agência.</p>
@@ -27,45 +26,43 @@
 						</div>
 					</figcaption>
 				</a>
-				<div class = "over-image ale"></div>
+				<div class="over-image ale"></div>
 			</div>
 		</div>
-		<div class = "col-xs-12 col-sm-3 equipe-effect">
-			<div class = "individuo">
-				<a href = "#" class = "equipe_mask" style = "display: table; width: 100%; height: 630px; position: absolute; z-index: 10">
-					<figcaption style = "display: table-cell; vertical-align: middle">
+		<div class="col-xs-12 col-sm-3 equipe-effect">
+			<div class="individuo">
+				<a href="#" class="equipe_mask">
+					<figcaption style="display: table-cell; vertical-align: middle">
 						<img src="<?php bloginfo('stylesheet_directory');?>/img/equipe/icone_dani.png" style="margin-bottom: 90px">
 						<div class="wrap">
 							<p style="margin-bottom: 90px">Com mais de 15 anos de experiência desde, comunicação visual, pré impressão, pós produção e criação. Hoje, é responsável pela direção de arte de grande parte  dos trabalhos da agência, seja, criação ou projetos web/digital.</p>
-							<div class = "equipe-desc"><h2>Daniel Mota</h2><p>Diretor de Arte / Web</p></div>
+							<div class="equipe-desc"><h2>Daniel Mota</h2><p>Diretor de Arte / Web</p></div>
 						</div>
 					</figcaption>
 				</a>
-				<div class = "over-image dani"></div>
+				<div class="over-image dani"></div>
 			</div>
 		</div>
-		<div class = "col-xs-12 col-sm-3 equipe-effect">
-			<div class = "individuo">
-				<a href = "#" class = "equipe_mask" style = "display: table; width: 100%; height: 630px; position: absolute; z-index: 10">
-					<figcaption style = "display: table-cell; vertical-align: middle">
+		<div class="col-xs-12 col-sm-3 equipe-effect">
+			<div class="individuo">
+				<a href="#" class="equipe_mask">
+					<figcaption style="display: table-cell; vertical-align: middle">
 						<img src="<?php bloginfo('stylesheet_directory');?>/img/equipe/icone_marq.png" style="margin-bottom: 90px">
 						<div class="wrap">
 							<p style="margin-bottom: 90px">Formado em desenvolvimento de sistemas, é parte funcional para todos os projetos de web da agência, responsável por pesquisar, testar e implantar tudo o que acontece de novo no mundo digital, envolvimento pleno em todo processo criativo, quando o assunto é digital.</p>
-							<div class = "equipe-desc"><h2>Marcus Gonçalves</h2><p>Assistênte Web</p></div>
+							<div class="equipe-desc"><h2>Marcus Gonçalves</h2><p>Assistênte Web</p></div>
 						</div>
 					</figcaption>
 				</a>
-				<div class = "over-image marq"></div>
+				<div class="over-image marq"></div>
 			</div>
 		</div>
 	</div><!-- container -->
 </div><!-- equipe -->
-
 <div class="container mundo">
 	<div style="text-align: center;">
 		<span class="title"><strong>a mundo</strong></span>
 	</div>
-
 	<div class="row">
 		<div class="col-sm-12 col-md-4">
 			<div class="thumbnail">
@@ -78,7 +75,7 @@
 			<div class="thumbnail">
 				<div class="caption">
 					<p>Nosso Portfólio apresenta bem todos os projetos no qual nos envolvemos, começando pela diversidade de segmentos: alimentação, cosméticos, automotivo, serviços, engenharia, consultorias, entre outros.
-Nosso trabalho começa desde a reestilização de logotipo, ao posicionamente da empresa ou produtoo no mercado.</p>
+					Nosso trabalho começa desde a reestilização de logotipo, ao posicionamente da empresa ou produtoo no mercado.</p>
 				</div>
 			</div>
 		</div>
@@ -89,12 +86,9 @@ Nosso trabalho começa desde a reestilização de logotipo, ao posicionamente da
 				</div>
 			</div>
 		</div>
-	
-		
 		<div style="text-align: center;">
 			<span class="title"><strong>clientes</strong></span>
 		</div>
-
 		<div class="col-sm-12 mundo_brands">
 			<img src="<?php bloginfo('stylesheet_directory');?>/img/brands/logopalacio.png" class="img-responsive" alt="Palácio do Pão">
 			<img src="<?php bloginfo('stylesheet_directory');?>/img/brands/logolobo.png" class="img-responsive" alt="Lobo Assessoria">
@@ -113,9 +107,6 @@ Nosso trabalho começa desde a reestilização de logotipo, ao posicionamente da
 			<img src="<?php bloginfo('stylesheet_directory');?>/img/brands/logovcl.png" class="img-responsive" alt="VCL Serviços">
 			<img src="<?php bloginfo('stylesheet_directory');?>/img/brands/logotheodora.png" class="img-responsive" alt="Theodora Confeitaria">
 		</div>
-
 	</div> <!-- row -->
-
 </div> <!-- container -->
-
 <?php get_footer(); ?>
